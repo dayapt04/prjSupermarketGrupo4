ced8f727fd34ee9e62d5f49cbbb58d005a5615d2
Riamu360 <liamzuniga360@gmail.com>
2023-12-16 23:25:07 -0500

    Producto: G2

--- src/Producto.java
+++ src/Producto.java
@@ -71,21 +71,21 @@ public class Producto {
     }
 
     public void listaProductos(){
-        Producto producto1 = new Producto(1, "001AMN","Agua", 0.50f, 0.60f, 40);
-        Producto producto2 = new Producto(2, "002CCG","Gaseosa", 0.50f, 0.65f, 40);
-        Producto producto3 = new Producto(3, "003PDI","Pan", 0.50f, 0.60f, 25);
-        Producto producto4 = new Producto(4, "004HRN","Harina", 0.70f, 0.60f, 40);
-        Producto producto5 = new Producto(5, "005AZR","Azucar", 0.80f, 0.60f, 50);
-        Producto producto6 = new Producto(6, "006ACE","Aceite", 0.50f, 0.60f, 50);
-        Producto producto7 = new Producto(7, "007PHS","Huevos", 0.50f, 0.60f, 30);
-        Producto producto8 = new Producto(8, "008ESM","Salsas", 0.50f, 0.60f, 55);
-        Producto producto9 = new Producto(9, "009PLL","Pollo", 0.50f, 0.60f, 30);
-        Producto producto10 = new Producto(10, "010CRN","Carne", 0.50f, 0.60f, 30);
-        Producto producto11 = new Producto(11, "011PZM","Pescado", 0.50f, 0.60f, 30);
-        Producto producto12 = new Producto(12, "012GDO","Galleta", 0.50f, 0.60f, 50);
-        Producto producto13 = new Producto(13, "013TMT","Tomate", 0.50f, 0.60f, 30);
-        Producto producto14 = new Producto(14, "014CBL","Cebolla", 0.50f, 0.60f, 30);
-        Producto producto15 = new Producto(15, "015FTS","Frutas", 0.50f, 0.60f, 50);
+        Producto producto1 = new Producto(1, "001AMN","Agua", 0.40f, 0.35f, 40);
+        Producto producto2 = new Producto(2, "002CCG","Gaseosa", 0.64f, 0.57f, 40);
+        Producto producto3 = new Producto(3, "003PDI","Pan", 2.03f, 1.77f, 25);
+        Producto producto4 = new Producto(4, "004HRN","Harina", 2.19f, 1.97f, 40);
+        Producto producto5 = new Producto(5, "005AZR","Azucar", 1.01f, 0.96f, 50);
+        Producto producto6 = new Producto(6, "006ACE","Aceite", 4.95f, 2.97f, 50);
+        Producto producto7 = new Producto(7, "007PHS","Huevos", 2.89f, 2.62f, 30);
+        Producto producto8 = new Producto(8, "008ESM","Salsas", 2.18f, 1.89f, 55);
+        Producto producto9 = new Producto(9, "009PLL","Pollo", 7.24f, 5.98f, 30);
+        Producto producto10 = new Producto(10, "010CRN","Carne", 7.09f, 5.87f, 30);
+        Producto producto11 = new Producto(11, "011ATN","Atun", 1.51f, 1.27f, 30);
+        Producto producto12 = new Producto(12, "012GDO","Galleta", 1.81f, 1.39f, 50);
+        Producto producto13 = new Producto(13, "013TMT","Tomate", 1.74f, 1.54f, 60);
+        Producto producto14 = new Producto(14, "014CBL","Cebolla", 1.58f, 1.34f, 60);
+        Producto producto15 = new Producto(15, "015LMN","Limones", 1.78f, 1.49f, 50);
         producto[0] = producto1;
         producto[1] = producto2;
         producto[2] = producto3;
