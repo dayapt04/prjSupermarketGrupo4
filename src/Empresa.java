a9d7ed60c85165d33eb97f625dc34262c74e692a
Shimi <jorgea.torresm@hotmail.com>
2023-12-17 19:56:47 -0500

    Inclusion de la clase Empresa y presentacion

---
+++ src/Empresa.java
@@ -0,0 +1,39 @@
+public class Empresa {
+
+    /**
+     * @author Jorge Torres, Alexis Vasco
+     * 
+     * 
+     * 
+     */
+
+    // Presentar Empresa
+    // Presentar cajero
+    // Persona juridica o natural
+    // Con que vas a pagar?
+
+    private String nombreEmpresa = "Supermaxi Quito";
+    private String DireccionEmpresa = "Madrid y Av. 12 de Octubre";
+    private String RucEmpresa = "1790016919001";
+
+    public String getnombreEmpresa(){
+        return this.nombreEmpresa;
+    }
+
+    public String getDireccionEmpresa(){
+        return this.DireccionEmpresa;
+    }
+
+    public String getRucEmpresa() {
+        return RucEmpresa;
+    }
+
+    //Datos Empleados
+
+    public String Nombre1 = "Eduard Cachicuango";
+    public String Cedula1 = "1717879012";
+
+    public String Nombre2 = "Brisa Cando";
+    public String Cedula2 = "1809097865";
+
+}
